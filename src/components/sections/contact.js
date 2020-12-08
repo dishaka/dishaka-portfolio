eb35c35cb4e67634154afe773291303b2c133ab8
Dishaka Tarafdar <dishaka99@gmail.com>
2020-12-08 21:27:47 +0530

    updated get in touch

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -51,8 +51,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        I am currently looking for full-time opportunities post-graduation starting Fall 2021. If
+        you think this is a good fit or just want to say hi, my inbox is always open!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
