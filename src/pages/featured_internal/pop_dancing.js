6ec70d4346b7efb626281ddf5a061caa807da3c6
dishaka <75124457+dishaka@users.noreply.github.com>
2022-04-26 02:02:30 +0530

    updated pop dancing page

--- src/pages/featured_internal/pop_dancing.js
+++ src/pages/featured_internal/pop_dancing.js
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout } from '@components';
 import Pop_Dancing from '../../components/featured_internal/Pop_Dancing';
+import Pop_Color from '../../components/featured_internal/Pop_Color';
+import Pop_Problem from '../../components/featured_internal/Pop_Problem';
+import Pop_Empathy from '../../components/featured_internal/Pop_Empathy';
+import Pop_Journey from '../../components/featured_internal/Pop_Journey';
+import Pop_Description from '../../components/featured_internal/Pop_Description';
+import Pop_Outcome from '../../components/featured_internal/Pop_Outcome';
 
 
 const StyledMainContainer = styled.main`
@@ -13,6 +19,12 @@ const pop_dancing = ({ location }) => (
     <div style={{maxWidth: '100vw'}}>
       <Layout location={location}>
         <Pop_Dancing />
+        <Pop_Color />
+        <Pop_Problem />
+        <Pop_Empathy />
+        <Pop_Journey />
+        <Pop_Description />
+        <Pop_Outcome />
   </Layout></div>
   );
 
