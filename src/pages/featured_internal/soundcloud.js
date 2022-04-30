3271be28f061a2bda577386c9b250ecbae6858b4
dishaka <75124457+dishaka@users.noreply.github.com>
2022-04-30 16:43:09 +0530

    added soundcloud page

---
+++ src/pages/featured_internal/soundcloud.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Layout } from '@components';
+import SoundCloud from '../../components/featured_internal/SoundCloud';
+import SoundCloud_Problem from '../../components/featured_internal/SoundCloud_Problem';
+import SoundCloud_Empathy from '../../components/featured_internal/SoundCloud_Empathy';
+import SoundCloud_Navigation from '../../components/featured_internal/SoundCloud_Navigation';
+
+const StyledMainContainer = styled.main`
+  counter-reset: section;
+`;
+
+const soundcloud = ({ location }) => (
+    <div style={{maxWidth: '100vw', overflow: 'hidden'}}>
+      <Layout location={location}>
+        <SoundCloud />
+        <SoundCloud_Problem />
+        <SoundCloud_Empathy />
+        <SoundCloud_Navigation />
+  </Layout>
+  </div>
+  );
+
+  soundcloud.propTypes = {
+    location: PropTypes.object.isRequired,
+  };
+  
+
+export default soundcloud
